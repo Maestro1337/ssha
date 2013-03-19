380cd5240f3e7600c2b6bf035e0b6f00076963cb
Maestro <adam.jilsen@gmail.com>
2013-03-19 14:00:45 +0100

    ShitZ n' Hoes

--- src/MainView.java
+++ src/MainView.java
@@ -1,7 +1,17 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
-
-public class MainView extends JFrame {
+public class MainView extends JFrame implements ActionListener {
 
 	public MainView() {
 		setSize(1024,768);
@@ -10,5 +20,32 @@ public class MainView extends JFrame {
 		setResizable(false);
 		
 		setVisible(true);
+		
+		JButton Ok = new JButton("OKAAAAAAAAAAAY!");
+		add(Ok);
+		Ok.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String fileName = "res/154561__ecfike__hurt-argh-1.wav";
+		if(e.getActionCommand() == "OKAAAAAAAAAAAY!")
+			playSound(fileName);
+			
+		
+	}
+	
+	public static synchronized void playSound(String filename) {
+
+		    try
+		    {
+		        Clip clip = AudioSystem.getClip();
+		        clip.open(AudioSystem.getAudioInputStream(new File(filename)));
+		        clip.start();
+		    }
+		    catch (Exception exc)
+		    {
+		        exc.printStackTrace(System.out);
+		    }	
 	}
 }
