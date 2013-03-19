7474b8acc26749c0dfe2d61861c7a34be2000cce
Maestro <adam.jilsen@gmail.com>
2013-03-19 13:15:02 +0100

    Skills n' shitZ

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,14 @@
 
 public class Player {
-
+	private int HP;
+	private String name;
+	private int damage;
+	private int movementSpeed;
+	private int armor;
+	private int atkSpeed;
+	private int range;
+	private int kills;
+	private int deaths;
+	private int coins;
+	private Skills[] skillList = new Skills[10];
 }
