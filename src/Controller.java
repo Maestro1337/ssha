1c56b4471f83d18f3d3b91edd8951b5d713eeee8
Maestro <adam.jilsen@gmail.com>
2013-03-19 13:20:15 +0100

    PenitZ

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,6 @@
 
 public class Controller {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-
+	
 }
--- src/Skills.java
+++ src/Skills.java
@@ -1,12 +1,6 @@
 
 public class Skills {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
