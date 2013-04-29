1f6bab8eb4b5001b19f1fd0fc01ee6f525b2130b
Rasmus <Rasmus@129.16.191.87>
2013-04-29 17:38:25 +0200

    Added dodge for AI

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -333,8 +333,16 @@ public class MainView extends BasicGameState implements ActionListener {
 		}
 		return value;
 	}
+	public boolean aiTimer (long delay){
+		return true;
+	}
+		
+	
 	
 	long time = 0;
+	int dodgedirX;
+	int dodgedirY;
+	
 	
 	public void AI(){
 		Random generator = new Random();
@@ -343,9 +351,8 @@ public class MainView extends BasicGameState implements ActionListener {
 		double dy = enemy.getY()-player.getY();
 		double distance = Math.sqrt((dx*dx)+(dy*dy));
 		while (System.currentTimeMillis()>time+delay){
-			if(Control.getCurrentActiveSkill().isAttacking()&&willCollide()){
-				enemyControl.move(0,0);
-				//TODO correct movecoordinate
+			if(Control.getCurrentActiveSkill().isAttacking()&&Control.getCurrentActiveSkill().getRange()>distance){
+				enemyControl.move((int)(enemy.getX()+dy),(int)(enemy.getY()-dx));
 				}
 				
 			
