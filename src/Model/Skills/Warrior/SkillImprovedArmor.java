18a6535877ab2dc0e4975f7d04334db7caf13b5a
Maestro <adam.jilsen@gmail.com>
2013-05-04 20:51:55 +0200

    Small fix.

--- src/Model/Skills/Warrior/SkillImprovedArmor.java
+++ src/Model/Skills/Warrior/SkillImprovedArmor.java
@@ -8,7 +8,7 @@ import Model.Skills.Skill;
 public class SkillImprovedArmor extends Skill {
 	public SkillImprovedArmor(){
 		//String name, int cd, int range, double speed, int aoe, int cost, int damage, StatusEffect SE
-				super("Improved armor", 11000, 400, 0.4, 3, 0, 150,"The wizard \n" +
+				super("Improved armor", 11000, 400, 0.4, 3, 0, 150,"The warrior \n" +
 						"Level 1: 15 damage\n" +
 						"Level 2: 25 damage\n" +
 						"Level 3: 35 damage\n" +
