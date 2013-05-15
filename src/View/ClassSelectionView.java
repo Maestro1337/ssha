77da11c337b9cf504c00e4fe87be4ce0d33e92f4
bellevik <bellevik@gmail.com>
2013-05-15 15:14:24 +0200

    Fixed error in view.

--- src/View/ClassSelectionView.java
+++ src/View/ClassSelectionView.java
@@ -87,7 +87,7 @@ public class ClassSelectionView extends BasicGameState implements ActionListener
 //				System.out.println(GlobalClassSelector.getController().getPlayers().size());
 				
 				//Addition of AI player
-				GlobalClassSelector.getController().addPlayer(new ClassWarrior("Enemy", 600, 600, 1), 1);
+				GlobalClassSelector.getController().addPlayer(new ClassWarrior("Enemy", "ai", 600, 600, 1), 1);
 //				System.out.println(GlobalClassSelector.getController().getPlayers().size());
 				
 				sbg.enterState(1);
@@ -102,7 +102,7 @@ public class ClassSelectionView extends BasicGameState implements ActionListener
 						"Your skills with weapons in close combat makes you a powerful \n" +
 						"force on the battlefield and a durable opponent for all who dares \n" +
 						"cross your way.";
-				player = new ClassWarrior("Player", 120, 100, 0);
+				player = new ClassWarrior("Player", "player", 120, 100, 0);
 		//		Control = new PlayerController("WarriorMan", obsGenerator.nextInt(1280), obsGenerator.nextInt(719) + 1, obstacles, "Warrior");
 			}
 		}
@@ -113,7 +113,7 @@ public class ClassSelectionView extends BasicGameState implements ActionListener
 						"their opponents. Able to take down tyrants without blinking an eye \n" +
 						"or breaking a bowstring, you'll range far and wide with this class.";
 		//		classType = "Hunter";
-				player = new ClassHunter("Player", 120, 100, 0);
+				player = new ClassHunter("Player", "player", 120, 100, 0);
 		//		Control = new PlayerController("HunterMan", obsGenerator.nextInt(1280), obsGenerator.nextInt(719) + 1, obstacles, "Hunter");
 			}
 		}
@@ -124,7 +124,7 @@ public class ClassSelectionView extends BasicGameState implements ActionListener
 						"a distance, able to cause great harm or restrict a targets actions using \n" +
 						"their supreme knowledge of the elements.";
 		//		classType = "Wizard";
-				player = new ClassWizard("Player", 120, 100, 0);
+				player = new ClassWizard("Player", "player", 120, 100, 0);
 		//		Control = new PlayerController("WizardMan", obsGenerator.nextInt(1280), obsGenerator.nextInt(719) + 1, obstacles, "Wizard");
 			}
 		}
