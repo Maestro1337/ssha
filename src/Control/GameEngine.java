a83a96ed73d2b1dcef2cecc189b1ac5b34a22983
bellevik <bellevik@gmail.com>
2013-05-16 12:47:37 +0200

    Just deleted some unnecessary rows.

--- src/Control/GameEngine.java
+++ src/Control/GameEngine.java
@@ -43,14 +43,8 @@ public class GameEngine extends StateBasedGame {
 	public static void main(String[] args) {
 		AppGameContainer agc;
 		try{
-			
-			
-			
 			agc = new AppGameContainer(new GameEngine(gameName));
 			agc.setDisplayMode(1280, 720, false);
-			
-			System.out.println("BAAAAA1");
-			
 			agc.start();
 		}catch(SlickException ex){
 			ex.printStackTrace();
