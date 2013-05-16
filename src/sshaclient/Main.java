0347592e6b3e302444666bc919f2ecfc319758b4
bellevik <bellevik@gmail.com>
2013-05-16 13:22:01 +0200

    Added creation of SocketClient in GlobalClassSelector

--- src/sshaclient/Main.java
+++ src/sshaclient/Main.java
@@ -5,6 +5,8 @@ import Model.Classes.*;
 
 public class Main {
 	
+	/*
+	
 	public static void main(String[] args) {
 		
 		TestView tv = new TestView();
@@ -18,4 +20,6 @@ public class Main {
 		
 	}
 
+	*/
+	
 }
