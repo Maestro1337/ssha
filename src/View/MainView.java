891f056d706221f13b84c88ba0bd9514f41ea304
Maestro <adam.jilsen@gmail.com>
2013-05-17 21:01:22 +0200

    Fixed the problem. Like a true problemsolver.

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -261,14 +261,11 @@ public class MainView extends BasicGameState implements ActionListener {
 			//g.setColor(Color.white);
 			//g.fillRect(140 + j*64, 640, 64, 64);
 			g.setColor(Color.black);
-			
 			if(activePlayerSkills[j] != null){
-				
-				if(activePlayerSkills[j].checkCooldown() == activePlayerSkills[j].getCoolDown()){
+				//if(activePlayerSkills[j].checkCooldown() == activePlayerSkills[j].getCoolDown()){
 					g.drawImage(activePlayerSkills[j].getSkillBarImage(),140 + j*64, 640);
-				}
+				//}
 				g.drawString(""+activePlayerSkills[j].checkCooldown(), activePlayerSkills[j].getSkillBarImage().getWidth()/2 + 140 + j*64, 610);
-				
 			}
 		}
 		
