4734b47b686d66cdc450fd8e9dce4f5cf4b4778b
VitoInfinito <tomas@hasselquist.se>
2013-05-21 20:11:25 +0200

    Changed global name

--- src/Control/GlobalClassSelector.java
+++ src/Control/GlobalClassSelector.java
@@ -25,7 +25,7 @@ public class GlobalClassSelector {
 	private int activePlayer = 0;
 	private Arena mapSelected;
 	private int difficultySelected;
-	private String playerName = "NeinInDaPENIS";
+	private String playerName = "TomasDaMegaGaylordInSebbesYummyTushie";
 	
 	//Singleton
 	public static GlobalClassSelector getController() {
