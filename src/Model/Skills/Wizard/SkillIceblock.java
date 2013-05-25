356be736826e09bd059ffe228e9dca1e7916e789
Maestro <adam.jilsen@gmail.com>
2013-05-25 17:43:45 +0200

    Implemented Iceblock

--- src/Model/Skills/Wizard/SkillIceblock.java
+++ src/Model/Skills/Wizard/SkillIceblock.java
@@ -4,20 +4,25 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 import Model.Skills.Skill;
+import Model.StatusEffects.StatusEffectDodge;
+import Model.StatusEffects.StatusEffectIceBlock;
+import Model.StatusEffects.StatusEffectImmobilize;
 
 public class SkillIceblock extends Skill {
 
 	public SkillIceblock() {
 		//String name, int cd, int range, double speed, int aoe, int cost, int damage, String skillDescription
 		super("Ice block", 11000, 0, 0, 20, 50, 0,"Ice block:\nEncapsule yourself inside \nan iceblock to escape from \nharms way.\n" +
-				"\nLevel 1: 1 second\n" +
-				"Level 2: 2 second\n" +
-				"Level 3: 3 second\n" +
-				"Level 4: 4 second");
+				"\nLevel 1: 2 second\n" +
+				"Level 2: 3 second\n" +
+				"Level 3: 4 second\n" +
+				"Level 4: 5 second");
 		
 		Image attackImage = null;
 		Image[] animation = new Image[1];
 		Image[] skillBar = new Image[3];
+		
+		super.setSelfAffectingStatusEffectShell(new StatusEffectIceBlock(this, 2));
 		
 		try {
 			attackImage = new Image("res/animations/iceblock/Iceblock.png");
@@ -35,7 +40,7 @@ public class SkillIceblock extends Skill {
 		
 		
 		super.setImage(attackImage);
-		super.setEndState(animation, 200, 400);
+		super.setEndState(animation, 5000, 5000);
 		super.setSkillBarImages(skillBar);
 	}
 
@@ -50,12 +55,15 @@ public class SkillIceblock extends Skill {
 			
 			switch(super.getCurrentLvl()){
 			case 2:
+				super.setSelfAffectingStatusEffectShell(new StatusEffectIceBlock(this, 3));
 				super.setDamage(lvl2);
 				break;
 			case 3:
+				super.setSelfAffectingStatusEffectShell(new StatusEffectIceBlock(this, 4));
 				super.setDamage(lvl3);
 				break;
 			case 4:
+				super.setSelfAffectingStatusEffectShell(new StatusEffectIceBlock(this, 5));
 				super.setDamage(lvl4);
 				break;
 			}
